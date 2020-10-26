29fb512f0c378f475a4fabaf11d755a8614e2458
L-AB <lesinternets@tutanota.com>
2020-10-26 19:48:50 +0100

    add auth

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const { graphqlHTTP } = require('express-graphql');
 
 const graphQlSchema = require('./src/graphql/schema');
 const graphQlResolvers = require('./src/graphql/resolvers');
-
+const isAuth = require('./src/middlewares/isAuth');
 
 const server = express();
 
 
 server.use(bodyParser.json());
 
+server.use(isAuth);
 
 server.use('/api', graphqlHTTP({
   schema: graphQlSchema,
